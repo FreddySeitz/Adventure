2b4ef4038fe0a77dcf7c0405ce1f756a3dd3d71b
Freddy <rseitz@ycp.edu>
2018-03-28 13:27:04 -0400

    Initial code for Command and GameEngine classes

---
+++ src/ycp/edu/cs320/adventure/game/GameEngine.java
@@ -0,0 +1,37 @@
+package ycp.edu.cs320.adventure.game;
+
+import java.util.ArrayList;
+
+// Operates on the current Game object
+public class GameEngine {
+	private ArrayList<Command> validCommands;
+	
+	public GameEngine() {
+		
+	}
+	
+	// Ends the game, incorporating checks for winning
+	public void endGame() {
+		
+	}
+	
+	// Loads a game
+	public Game loadGame(int game_id) {
+		return null;
+	}
+	
+	// Prompts the user for a new command
+	public void prompt(String command) {
+		
+	}
+	
+	// Stores the current Game object in the database
+	public void saveGame(){
+		
+	}
+	
+	// Updates the current Game object
+	public void update() {
+		
+	}
+}
