4b07e1098f13469a8029f45c923ea4b35f485926
AJ Loehr <ajloehr7@gmail.com>
2018-04-13 09:28:50 -0400

    very basic account creation logic

--- src/ycp/edu/cs320/adventure/servlets/CreateAccountServlet.java
+++ src/ycp/edu/cs320/adventure/servlets/CreateAccountServlet.java
@@ -2,12 +2,14 @@ package ycp.edu.cs320.adventure.servlets;
 
 import java.io.IOException;
 
+import javax.script.ScriptException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import ycp.edu.cs320.adventure.database.FakeDatabase;
+import ycp.edu.cs320.adventure.game.Account;
 
 public class CreateAccountServlet extends HttpServlet{
 private static final long serialVersionUID = 1L;
@@ -26,6 +28,34 @@ private static final long serialVersionUID = 1L;
         
         System.out.println("CreateAccount Servlet: doPost");
         String errorMessage = null;
+        String successMessage = null;
+        
+        Account account = new Account();
+        
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        String password2 = req.getParameter("password2");
+        
+        account.setUsername(username);
+        account.setPassword(password);
+        
+        // TODO: If database contains username give error message
+        
+        System.out.println("Password1: " + password);
+        System.out.println("Password2: " + password2);
+        
+        //else{
+        if(password.equals(password2)) {
+        	successMessage = "Account creation successful";
+        	req.setAttribute("successMessage", successMessage);
+        	req.getRequestDispatcher("/_view/createAccount.jsp").forward(req, resp);
+        	// Also redirect to game.jsp
+        }
+        else {
+        	errorMessage = "Error: Account already exists or passwords do not match.";
+        	req.setAttribute("errorMessage", errorMessage);
+        	req.getRequestDispatcher("/_view/createAccount.jsp").forward(req, resp);
+        }
         
     }
         
---
+++ war/_view/createAccount.jsp
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn" %>
+<html>
+	<head>
+		<title>Adventure - Create Account</title>
+		<style type="text/css">
+		.error {
+			color: red;
+		}
+		
+		td.label {
+			text-align: right;
+		}
+		.centered {
+  			position: fixed;
+  			top: 50%;
+  			left: 50%;
+  			/* bring your own prefixes */
+  			transform: translate(-50%, -50%);
+		}
+		.success{
+			color:green;
+			}
+		</style>
+	</head>
+
+	<body>
+		<div class = "centered">
+		<h1>Adventure Account Create</h1>
+		<c:if test="${! empty errorMessage}">
+			<div class="error">${errorMessage}</div>
+		</c:if>
+		<c:if test="${! empty successMessage}">
+			<div class="success">${successMessage}</div>
+		</c:if>
+		<!-- TODO: INDEX SERVLET -->
+		
+		<form action="${pageContext.servletContext.contextPath}/createAccount" method="post">
+			
+			<table>
+				<tr>
+					<td class="label">USERNAME: </td>
+					<td><input type="text" name="username" size="12" value="${username}" /></td>
+				</tr>
+				<tr>
+					<td class="label">PASSWORD: </td>
+					<td><input type="password" name="password" size="12" value="${password}" /></td>
+				</tr>
+				<tr>
+					<td class="label">CONFIRM PASSWORD: </td>
+					<td><input type="password" name="password2" size="12" value="${password2}" /></td>
+				</tr>
+				
+				
+			</table>
+			<input type="Submit" name="signin" value="Sign In">
+		
+			
+			</div>
+			</form>
+	</body>
+</html>
