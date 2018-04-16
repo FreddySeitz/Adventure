8e552941074e7cdd93a0c586be9678f6bdfe0bce
Freddy <rseitz@ycp.edu>
2018-04-16 09:29:22 -0400

    Changed Command class and started JUnit for it

--- src/ycp/edu/cs320/adventure/game/Command.java
+++ src/ycp/edu/cs320/adventure/game/Command.java
@@ -2,9 +2,31 @@ package ycp.edu.cs320.adventure.game;
 
 // Command class file
 public class Command{
+	private String command;
+	private String description;
 	
 	// Constructor
 	public Command() {
-		
+		command = "";
+	}
+	
+	// Sets command
+	public void setCommand(String command) {
+		this.command = command;
+	}
+	
+	// Gets command
+	public String getCommand() {
+		return command;
+	}
+	
+	// Sets description
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	// Gets command
+	public String getDescription() {
+		return description;
 	}
 }
